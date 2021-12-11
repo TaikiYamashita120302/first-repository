806382fc8a261689c614a0396fdfba88152d4a73
kiryutaiyo <taikinniku03@gmail.com>
2021-12-11 13:50:13 +0900

    課題8-2

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(Post $post) #Post $post → $post = new post　$postをclass Postでインスタンス化
+    {
+        dd($post->getPaginateByLimit(5));
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit(1)]);  
+    }
+}
+
+
+/*
+
+view関数はviewsフォルダの中のファイルを指定してそのファイルの中身を見せる関数
+別のどのファイルから指定しても飛べる、現にHttpの中のPostControllerから飛んでいる
+
+withの'posts'は配列でdd($post->getPaginateByLimit(2));で調べ、itemsを見ると分かるが、
+App\postが入っている。これがデータベースで作った中身。ちなみに2個しかないよ。
+
+そして右側$postが、3行上で自分で作成したindex関数の中の$post
+index関数内のPost関数はPost.phpで定めたクラス=postというクラスを参照してね、ということ、
+つまりpost.phpでみれるpostクラスをインスタンス化し、$postとした。
+その結果、getPaginateByLimitを使えるようになった。
+
+index.blade.php内の$postsはPostControllerの'posts'と対応
+
+基本、web.phpでhttp~(固定部分＝あらかじめ決まっている部分),~(自分で決める部分)の「自分で決める部分」を自分で決めている。
+それが/としている。
+*/
+
+?>
